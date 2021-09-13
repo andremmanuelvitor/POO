e0343a819ac7590d2bb3c8771bedadfb3af8ddfd
0xRadeon <andremmanuelvitor@gmail.com>
2021-09-13 14:25:10 -0300

    Create lab01.cpp
    Programa feito como introdução ao C++

---
+++ lab01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+string mensagem(string msg){
+  cout << msg;
+}
+
+int main()
+{
+  mensagem(1 – Converter Graus em Radianos);
+  /*
+  cout << "1 – Converter Graus em Radianos";
+  cout << "2 – Converter Reais em Dólares";
+  cout << "3 – Entrar com seu endereço completo";
+  cout << "4 - Sair do Programa";
+  cout << "Selecione uma opção(1-4):";
+  */
+  int ctd = 0;
+  /*
+  while (ctd != 4) {
+    switch (ctd) {
+      case 1: cout << "";
+      case 2:;
+      case 3:
+    }
+  }*/
+  return 0;
+}
